34feb6a74d2e81cb669ffd4208bff6ebca4ab477
Shantanu Gawande <shantanu.gawande02@gmail.com>
2026-08-31 16:24:09 +0530

    Question_1.py

--- FBS/Core Python/Tests/Test_1/Question_1.py
+++ FBS/Core Python/Tests/Test_1/Question_1.py
@@ -1,6 +1,6 @@
 length = int(input('Enter length:'))
 breadth = int(input('Enter breadth:'))
-radius = int(input('Enter radius:'))
+
 
 area = length*breadth+2*3.14*radius
 perimeter = 3.14*radius*radius 
